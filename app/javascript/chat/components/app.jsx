7754fa16a1d4b9cec1b45df5d02cb64e9951e5c9
yasonrails <yasbernays@gmail.com>
2022-10-27 14:41:40 +0200

    react chat

---
+++ app/javascript/chat/components/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ChannelList from '../containers/channel_list';
+import MessageList from '../containers/message_list';
+
+const App = () => {
+  return (
+    <div className="messaging-wrapper">
+      <div className="logo-container">
+        <img className="messaging-logo" src="assets/images/logo.svg" alt="logo" />
+      </div>     
+      <ChannelList selectedChannel={props.match.params.channel} />
+      <MessageList selectedChannel={props.match.params.channel} />
+    </div>
+  );
+};
+
+export default App;
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :channels, only: [] do
+        resources :messages, only: [ :index, :create ]
+      end
+    end
+  end
+  
+  resources :channels, only: [ :show ]
   # Defines the root path route ("/")
+  root to: 'channels#show'
   # root "articles#index"
 end
